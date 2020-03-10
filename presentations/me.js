5af17f4818894cf40c831d76a6270896f5cbd2e0
sharepointoscar <me@sharepointoscar.com>
2020-03-10 11:51:19 -0700

    deleted presos

--- presentations/me.js
+++
@@ -1,8 +0,0 @@
-{     
-    "me" : {
-         "name" : "Oscar Medina",
-         "twitter" : "@SharePointOscar",
-         "team" : "Jenkins X",
-	 "role" : "Developer Advocate | Jenkins X"
-     }
- }
--- presentations/workshops/continuous-delivery-jx/create-cluster.sh
+++
@@ -1,5 +0,0 @@
-# create cluster in GKE, replace <CLUSTER_NAME>
-# with whatever yours is.
-export CLUSTER_NAME='tequila' && \
-jx create cluster gke \
-    -n $CLUSTER_NAME --skip-installation --min-num-nodes=6 --max-num-nodes=8
